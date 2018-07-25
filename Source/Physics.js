d9ab778ff9ed6dc75790f6df8869a840cab2aff2
Mark Miller <coderush@gmail.com>
2018-07-25 11:01:42 -0600

    First code for the game after episode 1.

---
+++ Source/Physics.js
@@ -0,0 +1,10 @@
+class Physics {
+	constructor() {
+		
+  }
+  // All time units will be in seconds...
+  // All distance units will be in meters...
+  static getDisplacement(initialVelocity, time, acceleration) {
+    return initialVelocity * time + acceleration * time * time / 2;
+  }
+}
